eef032711f6e5b828e67bc93544ef71f5731aaa5
Akim <savvinakim@gmail.com>
2023-03-28 19:33:57 +0700

    Fixed timestamps

--- api/base_functions.py
+++ api/base_functions.py
@@ -7,7 +7,7 @@ def time_to_seconds(time):
     seconds = 0
     for i, element in enumerate(listtime):
         seconds += int(element) * 60 ** i
-    return str(seconds - 3187.5)
+    return str(seconds - 3187.4)
 
 
 def add_to_db():
